5491c1fa5949901db14550b52efe429209c9b8b8
Nelson <aylanelson01@gmail.com>
2024-11-19 18:56:22 -0800

    frontend stx TODO localhost fx testing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from '@chakra-ui/react'
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.render(
+  <React.StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </StrictMode>,
+  </React.StrictMode>,
+  document.getElementById('root'),
 )
